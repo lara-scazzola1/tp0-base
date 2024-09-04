bc8c7c7e882a896f6364703d962f0e03b5193baf
lara-scazzola1 <lscazzola@fi.uba.ar>
2024-09-03 23:10:48 -0300

    se cambia == por =

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -14,7 +14,7 @@ fi
 RESPONSE=$(docker run --network="${NETWORK_NAME}" --rm busybox sh -c \
   "echo '${MESSAGE}' | nc ${SERVER_CONTAINER_NAME} ${SERVER_PORT}" | tail -n 1)
 
-if [ "${RESPONSE}" == "${MESSAGE}" ]; then
+if [ "${RESPONSE}" = "${MESSAGE}" ]; then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"
